6f25467ef1442f7a32f054cd5da70307385f35ee
Gabriel <gabon95@o2.pl>
2020-11-25 17:53:01 +0100

    Fixed add merhod

--- src/main/java/com/gabi/jenxample/JenxampleApplication.java
+++ src/main/java/com/gabi/jenxample/JenxampleApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenxampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenxampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenxampleApplication.class, args);
+    }
 
-	public int add(int a, int b){
-		return a*b;
-	}
+    public int add(int a, int b) {
+        return a + b;
+    }
 
 }
